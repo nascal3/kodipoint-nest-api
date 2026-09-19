d7136422d28cf46e25fd4078629f0d2862f96dec
caleb <nascal3@gmail.com>
2026-09-19 03:23:53 +0300

    Fix issues causing app to crush when running it.

--- src/tenants/tenants.module.ts
+++ src/tenants/tenants.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { TenantsService } from './tenants.service';
 import {TenantsController} from "./tenants.controller";
+import {AuthModule} from "@/auth/auth.module";
 
 @Module({
+    imports: [AuthModule],
     controllers: [TenantsController],
     providers: [TenantsService],
     exports: [TenantsService],
